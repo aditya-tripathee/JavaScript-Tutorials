2ea39997f7b4629145393c1478aa7fd283eeac9c
Aditya Tripathi <145194957+aditya-tripathee@users.noreply.github.com>
2023-11-01 07:56:33 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,45 @@
+/*Section 2 - Data Types in JavaScript */
+
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs not browser
+
+console.log(3 + 3); 
+
+console.log("Aditya");
+
+
+let name = "adii"
+let age = 19
+let isLoggedIn = false 
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true , false 
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+
+//  object
+
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
