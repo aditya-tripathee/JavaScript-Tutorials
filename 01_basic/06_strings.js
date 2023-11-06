e94066e12b0729e06b6f49f0fa3f8fbf0656f2c1
Aditya Tripathi <145194957+aditya-tripathee@users.noreply.github.com>
2023-11-06 09:49:05 +0000

    Strings

---
+++ 01_basic/06_strings.js
@@ -0,0 +1,42 @@
+
+const name = "aditya "
+const repoCount = 50
+//console.log(name + repoCount + " value");
+ 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+
+const gameName = new String('Aditya-Tripathi')
+
+console.log(gameName[3]);
+console.log(gameName._prototype_);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 6)
+console.log(newString);
+
+
+const anotherString = gameName.slice(0, 8)
+console.log(anotherString);
+
+
+const newStringOne = " aditya "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https:www.adityatripathee.com"
+console.log(url.replace('www.', '//'));
+console.log(url.includes('tripathee'));
+console.log(url.split('-'));
+
+
+
+
+
+
+
+
