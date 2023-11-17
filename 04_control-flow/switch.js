c26a8b81b3aee685c729815d196e1ae2ca1c8a24
Aditya Tripathi <145194957+aditya-tripathee@users.noreply.github.com>
2023-11-17 07:15:38 +0000

    Switch case

---
+++ 04_control-flow/switch.js
@@ -0,0 +1,36 @@
+
+//  Switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "april"
+
+switch (month) {
+    case "Jan":
+        console.log("Jan");
+         break;
+    case "Feb":
+        console.log("Feb");
+         break;
+    case "March":
+        console.log("March");
+        //  break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+
