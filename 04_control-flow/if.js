ac790e26d955acf546bc57a9c1710500d742c901
Aditya Tripathi <145194957+aditya-tripathee@users.noreply.github.com>
2023-11-17 07:15:19 +0000

    If case

---
+++ 04_control-flow/if.js
@@ -0,0 +1,56 @@
+
+// If
+const isUserLoggedIn = true
+const temperature = 41
+
+if (temperature === 50){
+    console.log("less than 50");
+} else {
+console.log("temperature is greater than 50");
+}
+
+
+//  < less than , >  greater than , <= less than or equal , >=  greater than or equal , 
+// ==  double equal  , != not equal , === type check  
+
+
+// const score = 200 
+
+// if (score > 100) {
+//     var power = "fly"
+//     console.log(`Use power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+
+const balance = 1000
+ 
+if(balance < 500){
+ console.log("less than 500");
+
+}else if(balance < 750);{
+    console.log("less than 750");
+}
+
+
+const isUserLogIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+const GuestUser = true
+
+if (isUserLogIn && debitCard){
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail || GuestUser){
+    console.log("User logged in");
+}
+
+
+
+
+
+
+
