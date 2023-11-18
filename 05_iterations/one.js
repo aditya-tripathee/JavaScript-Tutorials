7c6f5963f64836524230a3672899a29f69e9acd1
Aditya Tripathi <145194957+aditya-tripathee@users.noreply.github.com>
2023-11-18 05:30:40 +0000

    Iterations

---
+++ 05_iterations/one.js
@@ -0,0 +1,69 @@
+
+//  For 
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    // console.log(element);
+     if (element == 5) {
+        console.log("6 is the best number");
+        
+     }
+   console.log(element);
+}
+
+// console.log(element);
+   
+
+   
+
+
+
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <= 10 ; j++) {
+    // console.log(`Inner loop value : ${j} and inner loop ${i}`);
+        console.log( i + '*' + j + '=' + i*j);
+    }
+    
+}
+
+
+
+let myArray = ["flash", "batman", "superman", "shaktimann"]
+console.log(myArray.length);
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+}
+
+
+//  break and continue 
+
+// for (let i = 0; i <= 20; i++) {
+   
+//      if (i == 5) {
+//       console.log(`Detected 5`);  
+//       break      
+//      }
+//     console.log(`value of i is ${i}`);
+    
+// }
+
+
+
+
+
+for (let i = 0; i <= 20; i++) {
+   
+     if (i == 5) {
+      console.log(`Detected 5`);  
+      continue     
+     }
+    console.log(`value of i is ${i}`);
+    
+}
+
+
---
+++ 05_iterations/two.js
@@ -0,0 +1,34 @@
+
+let index = 0 
+
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);  
+    index = index + 7  
+}
+
+
+
+let myArray = [ "shaktimann" , "spiderman" , "superman"]
+
+let arr = 0 
+ while (arr < myArray.length) {
+  console.log(`value is ${myArray[arr]} `);    
+  arr = arr + 1
+ }
+
+
+
+ 
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10) {
+    
+}
+
+
+
+
+
